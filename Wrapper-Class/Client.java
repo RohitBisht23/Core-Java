39163449d0166b473c4de09ab2011fa2be48c9de
Rohit Bisht <127304460+RohitBisht23@users.noreply.github.com>
2024-09-12 16:47:15 +0530

    Wrapper-class

---
+++ Wrapper-Class/Client.java
@@ -0,0 +1,7 @@
+//Every primitive data type have Wrapper class
+public class Client {
+    public static void main(String[] args) {
+        Integer a = 43; //This is wrapper class of int data type
+        System.err.println(a);
+    }
+}
